836c1f694b75e33210e3c3898b8d5c2d06cd2d15
Tim <timdrudy@gmail.com>
2025-05-14 15:51:18 -0500

    made highlights fully dynamic

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -11,37 +11,21 @@ const Highlights = () => {
                 What Makes <span className='purple'>Movies</span> so Great?
             </h2>
             <div className="highlight__wrapper">
-                <Highlight icon={<FontAwesomeIcon icon="dollar-sign" />} 
-                title="Free to Use"
-                para="Explore our free database of titles"
+                <Highlight 
+                    icon={<FontAwesomeIcon icon="dollar-sign" />} 
+                    title="Free to Use"
+                    para="Explore our free database of titles"
                 />
-                <div className="highlight">
-                    <div className="highlight__img">
-                        
-                    </div>
-                    <h3 className='highlight__subtitle'></h3>
-                    <p className='highlight__para'>
-                        
-                    </p>
-                </div>
-                <div className="highlight">
-                    <div className="highlight__img">
-                        <FontAwesomeIcon icon="pizza-slice" />
-                    </div>
-                    <h3 className='highlight__subtitle'>Easy to Use</h3>
-                    <p className='highlight__para'>
-                        With a few quick clicks, find the info you're looking for!
-                    </p>
-                </div>
-                <div className="highlight">
-                    <div className="highlight__img">
-                        <FontAwesomeIcon icon="face-smile-beam" />
-                    </div>
-                    <h3 className='highlight__subtitle'>Fun to Use</h3>
-                    <p className='highlight__para'>
-                        Learn everything you want to know about your favorite films!
-                    </p>
-                </div>
+                <Highlight 
+                    icon={<FontAwesomeIcon icon="pizza-slice" />} 
+                    title="Easy to Use"
+                    para="With a few quick clicks, find the info you're looking for!"
+                />
+                <Highlight 
+                    icon={<FontAwesomeIcon icon="face-smile-beam" />} 
+                    title="Fun to Use"
+                    para="Learn everything you want to know about your favorite films!"
+                />
             </div>
         </div>
       </div>
