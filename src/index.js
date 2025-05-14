404c5a97fdc850f3ad8de16f9ee321156249d690
Tim <timdrudy@gmail.com>
2025-05-14 13:17:33 -0500

    added Nav component but got error

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faShoppingCart,faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+library.add(faBars, faShoppingCart, faTimes)
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
